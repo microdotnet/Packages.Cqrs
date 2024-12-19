ec484b11a2ea5bdb21eef163cd5fa51937641b1d
MicroDotNet <microdotnet@lnow.pl>
2024-12-19 22:50:38 +0100

    Add default key generation strategies.
    Work on #4

--- src/Cqrs.Factories.DependencyInjection/IQueryHandlerKeysStrategy.cs
+++ src/Cqrs.Factories.DependencyInjection/IQueryHandlerKeysStrategy.cs
@@ -2,7 +2,7 @@ namespace MicroDotNet.Packages.Cqrs.Factories.DependencyInjection
 {
     public interface IQueryHandlerKeysStrategy
     {
-        string CreateKey<TResult>(IQuery<TResult> query)
+        string? CreateKey<TResult>(IQuery<TResult> query)
             where TResult : class;
     }
 }
